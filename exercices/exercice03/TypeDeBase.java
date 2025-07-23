b6b7f86e76362320e8461d0066742128f9ea5510
Gaspar-Jean-Marc-emf <jean-marc.gaspar@studentfr.ch>
2025-07-23 03:32:23 +0200

    Delete exercices/exercice03/TypeDeBase.java

--- exercices/exercice03/TypeDeBase.java
+++
@@ -1,8 +0,0 @@
-public class TypeDeBase{
-    
-    public static void main(final String[] args) {
-        final int age = 16;
-        final String nom = "jean-marc";
-        System.out.println("je m'appelle " + nom + " et j'ai " + age + " ans");
-    }
- }
